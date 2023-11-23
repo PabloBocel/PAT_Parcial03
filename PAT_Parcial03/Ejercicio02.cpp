1e38a1c66499a4bf4265d7d916770b213473a7e5
Pablo <andresbocel0@gmail.com>
2023-11-23 15:24:35 -0600

    eeeenoc

--- PAT_Parcial03/Ejercicio02.cpp
+++ PAT_Parcial03/Ejercicio02.cpp
@@ -1,6 +1,36 @@
 #include "Ejercicio02.h"
+#include <unordered_map>
+#include <vector>
 
-vector<string>* Ejercicio02::findRepeatedDnaSequences(Node<char>* head)
-{
-    return nullptr;
+using namespace std;
+
+vector<string>* Ejercicio02::findRepeatedDnaSequences(Node<char>* head) {
+    unordered_map<string, int> seen;
+    vector<string>* result = new vector<string>();
+
+    Node<char>* current = head;
+    string sequence;
+
+    for (int i = 0; i < 10 && current != nullptr; ++i) {
+        sequence += current->value;
+        current = current->next;
+    }
+
+    if (sequence.size() == 10) {
+        seen[sequence]++;
+    }
+
+    while (current != nullptr) {
+        sequence.erase(sequence.begin());
+        sequence += current->value;
+
+        if (++seen[sequence] == 2) {
+            result->push_back(sequence);
+        }
+
+        current = current->next;
+    }
+
+    return result;
 }
+
