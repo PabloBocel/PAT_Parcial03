90c0b5f5b892ad6cb8beab178ce460796c338fe7
Pablo <andresbocel0@gmail.com>
2023-11-23 15:30:46 -0600

    creoqel3

--- PAT_Parcial03/Ejercicio03.cpp
+++ PAT_Parcial03/Ejercicio03.cpp
@@ -1,14 +1,32 @@
+
 #include "Ejercicio03.h"
+#include <unordered_map>
+#include <map>
 
-TimeMap::TimeMap()
-{
+using namespace std;
+
+TimeMap::TimeMap() {
 }
 
-void TimeMap::set(string key, string value, int timestamp)
-{
+void TimeMap::set(string key, string value, int timestamp) {
+    data[key].emplace(timestamp, move(value));
 }
 
-string TimeMap::get(string key, int timestamp)
-{
-	return "";
+string TimeMap::get(string key, int timestamp) {
+    auto it = data.find(key);
+    if (it == data.end()) {
+        return "";
+    }
+
+    const map<int, string>& values = it->second;
+
+    auto itValue = values.upper_bound(timestamp);
+
+    if (itValue != values.begin()) {
+        --itValue;
+        return itValue->second;
+    }
+
+    return "";
 }
+
--- PAT_Parcial03/Ejercicio03.h
+++ PAT_Parcial03/Ejercicio03.h
@@ -1,16 +1,22 @@
 #pragma once
 
 #include <string>
+#include <string>
+#include <unordered_map>
+#include <map>
 
 using std::string;
 
 class TimeMap
 {
 public:
-	TimeMap();
+    TimeMap();
 
-	void set(string key, string value, int timestamp);
+    void set(string key, string value, int timestamp);
 
-	string get(string key, int timestamp);
+    string get(string key, int timestamp);
+
+private:
+    std::unordered_map<string, std::map<int, string>> data;
 };
 
